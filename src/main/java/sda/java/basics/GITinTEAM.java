af8cd1cda760c086ded6e7267e120ec4d402f543
Pablo <PabeleczeQ@cos.pl>
2020-02-23 10:48:02 +0100

    DOdane nowej wersji

--- src/main/java/sda/java/basics/GITinTEAM.java
+++ src/main/java/sda/java/basics/GITinTEAM.java
@@ -1,7 +1,12 @@
 package sda.java.basics;
 
 public class GITinTEAM {
+
     public static void main(String[] args) {
         System.out.println("Siemano Gity");
+        System.out.println("Nowa wersja");
     }
+
 }
+
+
