bc24a6369bec5dbf8bcebb832ca48498e415d78a
Piotr <uzytkownik@kursuGit.pl>
2020-02-23 11:04:43 +0100

    Zmiana

--- src/main/java/sda/java/basics/GITinTEAM.java
+++ src/main/java/sda/java/basics/GITinTEAM.java
@@ -5,6 +5,7 @@ public class GITinTEAM {
     public static void main(String[] args) {
         System.out.println("Siemano Gity");
         System.out.println("Nowa wersja");
+        System.out.println("Nara");
     }
 
 }
